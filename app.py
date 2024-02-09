1092270673557956bf51379922b2bb2ff36a0329
jhonsidney <jhon.sidney@gmail.com>
2024-02-09 18:58:01 -0300

    transforming random data

--- app.py
+++ app.py
@@ -32,6 +32,64 @@ def obter_informacoes_deepsecurity():
     versions = ['1.0','1.1','1.2','1.3','1.4','1.5','1.6', '2.0','2.1','2.2','2.3','2.4','2.5','3.0' '3.1', '3.2', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0', '10.0', '11.0', '12.0', '13.0', '14.0', '15.0', '16.0', '17.0', '18.0', '19.0', '20.0', '21.0', '22.0', '23.0', '24.0', '25.0', '26.0', '27.0', '28.0', '29.0', '30.0']
 
 
+    computersList = []
+    dictionary ={}
+
+    data_e_hora_de_hoje = datetime.now()
+
+    # Formatar a saída para "ANO-MÊS-DIA HORA:MINUTO:SEGUNDO"
+    formato = "%Y-%m-%d %H:%M:%S"
+    dateResponse = data_e_hora_de_hoje.strftime(formato)
+
+    y =  random.randint(0, 30)
+
+    for i in range(0,y):
+        informacoes_deepsecurity = {"computers": {
+                "timestamp": ''+ dateResponse,
+                "hostName": ''+random.choice(host_names),
+                "displayName": ''+random.choice(server_types),
+                "description": ''+random.choice(server_descriptions),
+                "lastIPUsed": ''+random.choice(ip_addresses),
+                "platform": ''+random.choice(plat),
+                "groupID": ''+random.choice(numeric_tokens),
+                "type_attack": ''+random.choice(cyber_attack_types),
+                "policyID": ''+random.choice(four_digit_tokens),
+                "agentVersion": " "+ random.choice(versions),
+                "computerStatus": {
+                    "agentStatus": ''+random.choice(server_statuses),
+                    "agentStatusMessages": ''+random.choice(server_statuses),
+                    "applianceStatus": ''+random.choice(server_statuses),
+                    "applianceStatusMessages":''+random.choice(server_statuses)
+                },
+                "webReputation": {
+                    "state": ''+random.choice(server_statuses),
+                    "moduleStatus": {
+                        "agentStatus": ''+random.choice(server_statuses),
+                        "categories": ''+random.choice(categories),
+                        "applianceStatus":''+random.choice(server_statuses),
+                        "urls": ''+random.choice(urls),
+                        "applianceStatusMessage": "Módulo de Reputação na Web está inativo para o appliance",
+                        "actions": ''+random.choice(actions)
+                    }
+                },
+                "firewalls": {
+                    "state": ''+random.choice(server_statuses),
+                    "sources_ipsx": ''+random.choice(source_ipsx),
+                    "globalStatefulConfigurationID":''+random.choice(ordemx),
+                    "destination_ips" : ''+random.choice(destination_ips),
+                    "actions": ''+random.choice(actions),
+                    "protocols":''+random.choice(protocols),
+                    "ports": ''+random.choice(ports),
+                    "statefulConfigurationAssignments": {},
+                    "ruleIDs": ''+random.choice(numeric_tokens)
+                }
+            }}
+        computersList.append(informacoes_deepsecurity)
+
+
+    dictionary["data"] = computersList
+    return jsonify(dictionary)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
